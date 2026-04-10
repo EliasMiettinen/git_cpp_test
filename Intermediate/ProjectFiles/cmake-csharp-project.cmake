d7e6f75ca7e8309d58a5203eb6bf56ca5f8e04a3
Harald <miettinen.elias@yahoo.com>
2026-04-10 15:35:46 +0300

    Remove DerivedDataCache, Intermediate and saved from git tracking

--- Intermediate/ProjectFiles/cmake-csharp-project.cmake
+++
@@ -1,4 +0,0 @@
-set(PROJECT_CSHARP_FILES 
-	"/mnt/localvarasto/Documents/Projects/Unreal/cpp_git_test/Source/cpp_git_test/cpp_git_test.Build.cs"
- )
-
--- Intermediate/ProjectFiles/cmake-sources-project.cmake
+++
@@ -1,6 +0,0 @@
-set(PROJECT_SOURCE_FILES 
-	"/mnt/localvarasto/Documents/Projects/Unreal/cpp_git_test/Source/cpp_git_test/cpp_git_test.cpp"
-	"/mnt/localvarasto/Documents/Projects/Unreal/cpp_git_test/Source/cpp_git_test/cpp_git_testCharacter.cpp"
-	"/mnt/localvarasto/Documents/Projects/Unreal/cpp_git_test/Source/cpp_git_test/cpp_git_testGameMode.cpp"
- )
-
